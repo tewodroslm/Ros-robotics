ea8f748cd870cc0a708a8649e5b621d6e44fe10c
Surafel Mohammed <surafelmohammed66@gmail.com>
2021-05-22 23:10:13 +0300

    gezabo labs

---
+++ gazebolab/catkin_ws/build/arm_gazebo/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;gazebo_ros;arm_lib".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "arm_gazebo"
+PROJECT_SPACE_DIR = "/home/ted/Documents/Projects/ROS/gazebolab/catkin_ws/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ gazebolab/catkin_ws/build/arm_lib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ted/Documents/Projects/ROS/gazebolab/catkin_ws/src/arm_lib
+# Build directory: /home/ted/Documents/Projects/ROS/gazebolab/catkin_ws/build/arm_lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

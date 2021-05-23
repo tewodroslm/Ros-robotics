9a636c5058d6a5b9262b5fd3a3559dfd8f637885
gashayee <tewodroslemma64@gmail.com>
2021-05-23 18:54:30 +0300

    fix ye 4th solution

--- src/arm_gazebo/src/Listeners.cpp
+++ src/arm_gazebo/src/Listeners.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 
 // include file for the publisher to work
-#include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Float32MultiArray.h"
 #include <sstream>
@@ -15,6 +14,10 @@
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
 
+#include <thread>
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
 
 namespace gazebo {
  
@@ -25,8 +28,9 @@ namespace gazebo {
 	public:
         Listener(){}
     
-    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr /*_sdf*/)
-		{
+    public: 
+        virtual void Load(physics::ModelPtr _model, sdf::ElementPtr sdf)
+	 	   {
             
             // Safety check
             if (_model->GetJointCount() == 0)
@@ -38,44 +42,112 @@ namespace gazebo {
             // Store the model pointer for convenience.
             this->model = _model;
 
-            // Get the first joint. We are making an assumption about the model
-            // having one joint that is the rotational joint.
-            this->joint = _model->GetJoints()[0];
-
-            // Setup a P-controller, with a gain of 0.1.
-            this->pid = common::PID(0.1, 0, 0);
-
-            // Apply the P-controller to the joint.
-            this->model->GetJointController()->SetPositionTarget(
-                this->joint->GetScopedName(), this->pid);
-
-            // Set the joint's target velocity. This target velocity is just
-            // for demonstration purposes.
-            this->model->GetJointController()->SetPositionTarget(
-                this->joint->GetScopedName(), 10.0);
-
                 // Create the node
             this->node = transport::NodePtr(new transport::Node());
             
-            this->node->Init(this->model->GetWorld()->Name());
+            this->node->Init("talker");
         
 
             // Create a topic name
-            std::string topicName = "~/" + this->model->GetName() + "/list_cmd";
+            std::string topicName = "/chatter";
 
             // Subscribe to the topic, and register a callback
             this->sub = this->node->Subscribe(topicName,
-               &Listener::OnMsg, this);
+               &Listener::OnRosMsg, this);
+            
+            // Initialize ros, if it has not already bee initialized.
+            if (!ros::isInitialized())
+            {
+              int argc = 0;
+              char **argv = NULL;
+              ros::init(argc, argv, "gazebo_client",
+                  ros::init_options::NoSigintHandler);
+            }
+
+            // Create our ROS node. This acts in a similar manner to
+            // the Gazebo node
+            this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+
+            // Create a named topic, and subscribe to it.
+            // ros::SubscribeOptions so =
+            //   ros::SubscribeOptions::create<gazebo::msgs::Quaternion>(
+            //       "/chatter",
+            //       1,
+            //       boost::bind(&Listener::OnRosMsg, this, _1),
+            //       ros::VoidPtr(), &this->rosQueue);
+            // this->rosSub = this->rosNode->subscribe(so);
+
+
+            // Spin up the queue helper thread.
+            this->rosQueueThread =
+              std::thread(std::bind(&Listener::QueueThread, this));
 
 		}
+
+        public: void OnRosMsg(ConstQuaternionPtr &_msg)
+        {
+            // this->SetVelocity(_msg->data);
+            // std::cout << "Listener loaded  Number  => " << _msg->data <<std::endl;
+            
+            std::cout << _msg->x() << " " << _msg->y() << " " << _msg->z() << " " << _msg->w() << std::endl;
+            
+            this->pid = common::PID(20.1, 10.01, 10.03);
+
+            float angleDegree = _msg->w();
+			float rad = M_PI * angleDegree / 180;
+
+			std::string name1 = this->model->GetJoint("chasis_arm1_joint")->GetScopedName();
+			this->jointController->SetPositionPID(name1, pid);
+			this->jointController->SetPositionTarget(name1, rad);
+			this->jointController->Update();
+
+            float angleDegree1 = _msg->x();
+			float rad1 = M_PI * angleDegree1 / 180;
+
+			std::string name2 = this->model->GetJoint("arm1_arm2_joint")->GetScopedName();
+			this->jointController->SetPositionPID(name2, pid);
+			this->jointController->SetPositionTarget(name2, rad1);
+			this->jointController->Update();
+
+            float angleDegree2 = _msg->y();
+			float rad2 = M_PI * angleDegree2 / 180;
+
+			std::string name3 = this->model->GetJoint("arm2_arm3_joint")->GetScopedName();
+			this->jointController->SetPositionPID(name3, pid);
+			this->jointController->SetPositionTarget(name3, rad2);
+			this->jointController->Update();
+
+            float angleDegree3 = _msg->z();
+			float rad3 = M_PI * angleDegree3 / 180;
+
+			std::string name4 = this->model->GetJoint("arm3_arm4_joint")->GetScopedName();
+			this->jointController->SetPositionPID(name4, pid);
+			this->jointController->SetPositionTarget(name4, rad3);
+			this->jointController->Update();
+
+        }
 
         private: void OnMsg(ConstVector3dPtr &_msg)
         {
         //   this->SetVelocity(_msg->x());
-            ROS_INFO("I heard: [%s]", msg->data.c_str());
+            printf("Listener loaded \n");
+            //ROS_INFO("I heard: [%s]", msg->data.c_str());
+        }
+        
+        /// \brief ROS helper function that processes messages
+        private: void QueueThread()
+        {
+          static const double timeout = 0.01;
+          while (this->rosNode->ok())
+          {
+            this->rosQueue.callAvailable(ros::WallDuration(timeout));
+          }
         }
 
         private: physics::ModelPtr model;
+        
+        private:
+		    physics::JointControllerPtr jointController;
 
         /// \brief Pointer to the joint.
         private: physics::JointPtr joint;
@@ -89,6 +161,18 @@ namespace gazebo {
 
         /// \brief A subscriber to a named topic.
         private: transport::SubscriberPtr sub;
+
+        /// \brief A node use for ROS transport
+        private: std::unique_ptr<ros::NodeHandle> rosNode;
+
+        /// \brief A ROS subscriber
+        private: ros::Subscriber rosSub;
+
+        /// \brief A ROS callbackqueue that helps process messages
+        private: ros::CallbackQueue rosQueue;
+
+        /// \brief A thread the keeps running the rosQueue
+        private: std::thread rosQueueThread;
 		
 	};
     GZ_REGISTER_MODEL_PLUGIN(Listener)

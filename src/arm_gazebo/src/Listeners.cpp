336914f65275acb0d128ed058fea606cf5ddcf34
gashayee <tewodroslemma64@gmail.com>
2021-05-24 00:05:23 +0300

    final push

--- src/arm_gazebo/src/Listeners.cpp
+++ src/arm_gazebo/src/Listeners.cpp
@@ -31,7 +31,13 @@ namespace gazebo {
     public: 
         virtual void Load(physics::ModelPtr _model, sdf::ElementPtr sdf)
 	 	   {
-            
+
+            this->chasis_arm1 = 0;
+            this->arm1_arm2 = 0;
+            this->arm2_arm3 = 0;
+            this->arm3_arm4 = 0;
+
+
             // Safety check
             if (_model->GetJointCount() == 0)
             {
@@ -41,6 +47,9 @@ namespace gazebo {
 
             // Store the model pointer for convenience.
             this->model = _model;
+            this->jointController = this->model->GetJointController();
+
+            this->pid = common::PID(20.1, 10.01, 10.03);
 
                 // Create the node
             this->node = transport::NodePtr(new transport::Node());
@@ -68,62 +77,68 @@ namespace gazebo {
             // the Gazebo node
             this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
 
-            // Create a named topic, and subscribe to it.
-            // ros::SubscribeOptions so =
-            //   ros::SubscribeOptions::create<gazebo::msgs::Quaternion>(
-            //       "/chatter",
-            //       1,
-            //       boost::bind(&Listener::OnRosMsg, this, _1),
-            //       ros::VoidPtr(), &this->rosQueue);
-            // this->rosSub = this->rosNode->subscribe(so);
-
-
             // Spin up the queue helper thread.
             this->rosQueueThread =
               std::thread(std::bind(&Listener::QueueThread, this));
 
+
+            this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+				std::bind(&Listener::OnUpdate, this));
 		}
 
-        public: void OnRosMsg(ConstQuaternionPtr &_msg)
-        {
-            // this->SetVelocity(_msg->data);
-            // std::cout << "Listener loaded  Number  => " << _msg->data <<std::endl;
+
+        public:
+            void OnUpdate()
+            {
+
+            printf("ON OnUpdate -=-=-=-=    loaded \n");
             
-            std::cout << _msg->x() << " " << _msg->y() << " " << _msg->z() << " " << _msg->w() << std::endl;
             
-            this->pid = common::PID(20.1, 10.01, 10.03);
+            float angleDegree1 = this->arm1_arm2;
+		    float rad1 = M_PI * angleDegree1 / 180;
 
-            float angleDegree = _msg->w();
-			float rad = M_PI * angleDegree / 180;
-
-			std::string name1 = this->model->GetJoint("chasis_arm1_joint")->GetScopedName();
-			this->jointController->SetPositionPID(name1, pid);
-			this->jointController->SetPositionTarget(name1, rad);
-			this->jointController->Update();
-
-            float angleDegree1 = _msg->x();
-			float rad1 = M_PI * angleDegree1 / 180;
-
-			std::string name2 = this->model->GetJoint("arm1_arm2_joint")->GetScopedName();
-			this->jointController->SetPositionPID(name2, pid);
+		    std::string name2 = this->model->GetJoint("arm1_arm2_joint")->GetScopedName();
+			this->jointController->SetPositionPID(name2, this->pid);
 			this->jointController->SetPositionTarget(name2, rad1);
 			this->jointController->Update();
 
-            float angleDegree2 = _msg->y();
+            float angleDegree = this->chasis_arm1;
+			float rad = M_PI * angleDegree / 180;
+
+			std::string name1 = this->model->GetJoint("chasis_arm1_joint")->GetScopedName();
+			this->jointController->SetPositionPID(name1, this->pid);
+			this->jointController->SetPositionTarget(name1, rad);
+			this->jointController->Update();
+            
+            float angleDegree2 = this->arm2_arm3;
 			float rad2 = M_PI * angleDegree2 / 180;
 
-			std::string name3 = this->model->GetJoint("arm2_arm3_joint")->GetScopedName();
-			this->jointController->SetPositionPID(name3, pid);
-			this->jointController->SetPositionTarget(name3, rad2);
+		    std::string name3 = this->model->GetJoint("arm2_arm3_joint")->GetScopedName();
+		    this->jointController->SetPositionPID(name3, this->pid);
+		    this->jointController->SetPositionTarget(name3, rad2);
 			this->jointController->Update();
-
-            float angleDegree3 = _msg->z();
+            float angleDegree3 = this->arm3_arm4;
 			float rad3 = M_PI * angleDegree3 / 180;
 
 			std::string name4 = this->model->GetJoint("arm3_arm4_joint")->GetScopedName();
-			this->jointController->SetPositionPID(name4, pid);
+			this->jointController->SetPositionPID(name4, this->pid);
 			this->jointController->SetPositionTarget(name4, rad3);
 			this->jointController->Update();
+
+            }
+
+        public: void OnRosMsg(ConstQuaternionPtr &_msg)
+        {
+                        
+            std::cout << _msg->x() << " " << _msg->y() << " " << _msg->z() << " " << _msg->w() << std::endl;
+            
+
+            this->chasis_arm1 = _msg->w();
+            this->arm1_arm2 = _msg->x();
+            this->arm2_arm3 = _msg->y();
+            this->arm3_arm4 = _msg->z();
+            // this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+			// 	boost::bind(&Listener::OnUpdate, this));
 
         }
 
@@ -141,8 +156,13 @@ namespace gazebo {
           while (this->rosNode->ok())
           {
             this->rosQueue.callAvailable(ros::WallDuration(timeout));
-          }
+          }          
+
         }
+
+
+        private:
+		    event::ConnectionPtr updateConnection;
 
         private: physics::ModelPtr model;
         
@@ -173,6 +193,13 @@ namespace gazebo {
 
         /// \brief A thread the keeps running the rosQueue
         private: std::thread rosQueueThread;
+        
+        private:
+            float chasis_arm1;
+            float arm1_arm2;
+            float arm2_arm3;
+            float arm3_arm4;
+
 		
 	};
     GZ_REGISTER_MODEL_PLUGIN(Listener)
